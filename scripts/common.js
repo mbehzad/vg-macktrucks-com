011a7cbb0b197586a0616bfd550d90a96d1cb59f
mbehzad <mehran.behzad@diva-e.com>
2024-10-19 22:03:26 +0200

    reduce fetchpri of svg icons

--- scripts/common.js
+++ scripts/common.js
@@ -119,7 +119,7 @@ export async function decorateIcons(element) {
     if (!ICONS_CACHE[iconName]) {
       ICONS_CACHE[iconName] = true;
       try {
-        const response = await fetch(`${window.hlx.codeBasePath}/icons/${iconName}.svg`);
+        const response = await fetch(`${window.hlx.codeBasePath}/icons/${iconName}.svg`, { priority: 'low' });
         if (!response.ok) {
           ICONS_CACHE[iconName] = false;
           return;
